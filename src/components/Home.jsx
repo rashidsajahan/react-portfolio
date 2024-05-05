a6a5a80cd9ff37d107e3f7e237d6e368073186ff
rashidsajahan <rashidsajahan.gmail.com>
2024-05-05 22:06:29 +0530

    coma removed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
     <div name='home' className="w-full h-screen bg-[#011343]">
       {/* container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p className="text-pink-600">Hi, I'm Rashid,</p>
+        <p className="text-pink-600">Hi, I'm Rashid</p>
         <h1 className="text text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
           Rashid Sajahan
         </h1>
