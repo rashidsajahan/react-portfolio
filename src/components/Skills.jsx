d5d92edbaa625ccb300ec6608080a4ace14c5245
rashidsajahan <rashidsajahan.gmail.com>
2024-05-05 21:30:12 +0530

    alignment issue fixed

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,7 +13,7 @@ import Java from '../assets/java.png'
 function Skills() {
   return (
     <div name='skills' className='w-full h-full bg-[#011343] text-gray-300'>
-      <div className='max-w[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div>
           <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Experience</p>
           <p className='py-4'>These are the technologies I've worked with</p>
